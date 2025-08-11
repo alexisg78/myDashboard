d1ffd78b386d5ba5f4098ff7bd35655e91002872
Alexis Gonzalez <galexis.isi78@gmail.com>
2025-08-10 21:45:37 -0300

    Defer triggers

--- src/app/dashboard/pages/defer-options/defer-options.component.ts
+++ src/app/dashboard/pages/defer-options/defer-options.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { HeavyLoadersFastComponent } from '../../../shared/heavy-loaders/heavy-loaders-fast.component';
+import { TitleComponent } from '../../../shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [],
-  templateUrl: './defer-options.component.html',
-  styles: ``
+  imports: [CommonModule, HeavyLoadersFastComponent, TitleComponent],
+  templateUrl: './defer-options.component.html'
 })
 export default class DeferOptionsComponent {
+
+
 
 }
