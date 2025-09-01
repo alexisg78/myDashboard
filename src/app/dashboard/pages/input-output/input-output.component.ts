a6626f581cf88727a6b467fc75a12dd46a424d0b
Alexis Gonzalez <galexis.isi78@gmail.com>
2025-09-01 00:05:16 -0300

    Página Input - Output

---
+++ src/app/dashboard/pages/input-output/input-output.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ProductCardComponent } from './ui/product-card/product-card.component';
+
+@Component({
+  selector: 'app-input-output',
+  imports: [ ProductCardComponent ],
+  templateUrl: './input-output.component.html',
+  styles: ``
+})
+export default class InputOutputComponent {
+
+}
