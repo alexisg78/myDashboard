abc54f194608a992a579e4a0754d2b12411ccaa6
Alexis Gonzalez <galexis.isi78@gmail.com>
2025-09-02 22:47:10 -0300

    Path Angular Material

---
+++ src/app/dashboard/pages/material/material.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-material',
+  imports: [],
+  templateUrl: './material.component.html',
+  styles: ``
+})
+export default class MaterialComponent {
+
+}
